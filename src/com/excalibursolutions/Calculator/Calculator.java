3822d60619a30eef9e30299c04c69e8382f6b1a8
Robert Walsh <rwalsh@excalibur-solutions.com>
2023-02-18 17:58:04 -0500

    Initial commit

---
+++ src/com/excalibursolutions/Calculator/Calculator.java
@@ -0,0 +1,11 @@
+// Copyright(c)2023 Excalibur Solutions, Inc. - All Rights Reserved
+
+package com.excalibursolutions.Calculator;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		System.out.println("Starting the Calculator");
+	}
+
+}
