36ac551ede5fcc260c77770de41f853773b13346
Robert Walsh <rwalsh@excalibur-solutions.com>
2023-03-01 16:55:58 -0500

    Add ClearButton, Display to Calculator layout

--- src/com/excalibursolutions/Calculator/Calculator.java
+++ src/com/excalibursolutions/Calculator/Calculator.java
@@ -2,12 +2,39 @@
 
 package com.excalibursolutions.Calculator;
 
-public class Calculator {
+import javax.swing.BoxLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Calculator extends JFrame {
 
 	public static void main(String[] args) {
 		System.out.println("Starting the Calculator");
+		
+		new Calculator();
 	}
 
+	ClearButton clearButton;
+	Display display;
+	
+	public Calculator() {
+		super("Calculator");
+		
+		clearButton = new ClearButton(this);
+		display = new Display();
+		
+		JPanel row1 = new JPanel();
+		row1.add(clearButton.createGuiButton());
+		row1.add(display.createGuiComponent());
+		add(row1);
+		
+		setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+		setSize(250, 350);
+		setPreferredSize(getSize());
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+	
 	public void clear() {
 		
 	}
